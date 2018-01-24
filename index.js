e6aeed4d724783f21b83a8b4e3402c2ef718b956
jiashunming <jiashunming@outlook.com>
2018-01-24 15:06:26 +0800

    update: v2

--- index.js
+++ index.js
@@ -1,51 +1,118 @@
-'use strict';
+'use strict'
 
-var path = require('path');
-var winston = require('winston');
-var fs = require('fs');
-var mkdirp = require('mkdirp');
-var assign = require('object-assign');
+const path = require('path')
+const winston = require('winston')
+const fs = require('fs')
+const mkdirp = require('mkdirp')
 
-var createLogger = function(options) {
-  var logger = new (winston.Logger)({
-    transports: [
-      new (winston.transports.DailyRotateFile)(options)
-    ]
-  });
-  return logger;
-};
+const isLocal = process.env.NODE_ENV === 'local'
+let colors
+if (isLocal) {
+  colors = require('colors')
+}
 
-var alias = function(level) {
+let createLogger = options => new (winston.Logger)({
+  transports: [
+    new (winston.transports.DailyRotateFile)(options)
+  ]
+})
+
+let alias = level => {
   switch (level) {
     case 'access':
-      return 'info';
-      break;
+      return 'info'
     default:
-      return level;
+      return level
   }
 }
 
-module.exports = function(options) {
-  options = options || {};
-  var defaultOptions = {
-    app: 'app',
-    levels: ['error', 'access', 'debug'],
-    winston: {
-      datePattern: '-yyyy-MM-dd.log'
-    },
-    root: path.dirname(process.mainModule.filename)
+let defaultOptions = {
+  app: 'app',
+  levels: ['error', 'access', 'debug'],
+  winston: {
+    datePattern: '-yyyy-MM-dd.log'
+  },
+  root: path.dirname(process.mainModule.filename)
+}
+
+module.exports = (options = {}) => {
+  options = {
+    ...defaultOptions,
+    ...options
   }
-  options = assign(defaultOptions, options)
-  var logsPath = path.join(options.root, 'logs', options.app);
+  let logsPath = path.join(options.root, 'logs', options.app)
   if (!fs.existsSync(logsPath)) {
-    mkdirp.sync(logsPath);
+    mkdirp.sync(logsPath)
   }
-  var loggers = {};
-  options.levels.map(function(level) {
-    var winstonOptions = assign(options.winston, {
+  let loggers = {}
+  options.levels.forEach(level => {
+    let winstonOptions = {
+      ...options.winston,
       filename: path.join(logsPath, level)
-    });
-    loggers[level] = createLogger(winstonOptions)[alias(level)];
-  });
-  return loggers;
-};
+    }
+    loggers[level] = createLogger(winstonOptions)[alias(level)]
+  })
+
+  return loggerWrap(loggers)
+}
+
+/**
+ * 添加log格式化的处理
+ * @param  {object} logger 这个参数被废弃了 之前是传递进来logger对象
+ * @return {object}        返回error和access两个方法
+ */
+function loggerWrap (logger) {
+  return {
+    error (self, err, cusInfo) {
+      let req = self.request
+      let info = {
+        err_msg: err.message,
+        err_name: err.name,
+        err_stack: err.stack,
+        url: req.url,
+        href: req.href,
+        header: req.header,
+        ip: req.ip,
+        ips: req.ips
+      }
+      !isEmptyObject(cusInfo) && Object.assign(info, cusInfo)
+      isLocal && console.error(colors.red(JSON.stringify(info, null, 2)))
+      logger.error(info)
+    },
+    access (self, cusInfo) {
+      let req = self.request
+      let info = {
+        url: req.url,
+        href: req.href,
+        header: req.header,
+        ip: req.ip,
+        ips: req.ips
+      }
+      !isEmptyObject(cusInfo) && Object.assign(info, cusInfo)
+      isLocal && console.log(colors.green(JSON.stringify(info, null, 2)))
+      logger.access(info)
+    },
+    warn (self, cusInfo) {
+      let req = self.request
+      let info = {
+        url: req.url,
+        href: req.href,
+        header: req.header,
+        ip: req.ip,
+        ips: req.ips
+      }
+      !isEmptyObject(cusInfo) && Object.assign(info, cusInfo)
+      isLocal && console.log(colors.yellow(JSON.stringify(info, null, 2)))
+      logger.access(info)
+    }
+  }
+}
+
+/**
+ * 判断传入参数是否为空object
+ * @param  {object}  obj 目标对象
+ * @return {boolean}     true：为空对象
+ */
+function isEmptyObject (obj) {
+  return !obj || Object.keys(obj).length === 0
+}
